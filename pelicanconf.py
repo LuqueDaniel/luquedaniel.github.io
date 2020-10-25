a4fa48a38d2659e54cacf66edd184f38f41c4768
Daniel Luque <danielluque14@gmail.com>
2020-10-25 17:02:39 +0100

    Updated

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = "Daniel Luque Soria"
+SITENAME = "Portfolio - Daniel Luque"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "Europe/Madrid"
+
+DEFAULT_LANG = "es"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = {}
+
+# Social widget
+SOCIAL = {
+    "GitHub": "https://github.com/LuqueDaniel",
+    "Linkedin": "https://www.linkedin.com/in/daniel-luque-soria/",
+    "Email": "mailto:danielluque14@gmail.com",
+}
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+THEME = "theme"
